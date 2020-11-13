fe903659fdca7c656b258ea28c0fad145d5a477f
Wilhelm Kleiminger <wilhelm.kleiminger@ergon.ch>
2020-11-13 14:50:46 +0100

    Moves from support.appcompat to androix.appcompat

--- demo/src/main/java/kaufland/com/demo/MainActivity.kt
+++ demo/src/main/java/kaufland/com/demo/MainActivity.kt
@@ -1,7 +1,7 @@
 package kaufland.com.demo
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
