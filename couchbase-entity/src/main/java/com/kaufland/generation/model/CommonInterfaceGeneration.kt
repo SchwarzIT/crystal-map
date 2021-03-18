0e469766190aaaacadcd4d5b20f6d199da7feed2
Sven Braun <sven.braune@mail.schwarz>
2021-03-18 17:12:48 +0100

    # mapsupport for all generated interfaces

--- couchbase-entity/src/main/java/com/kaufland/generation/model/CommonInterfaceGeneration.kt
+++ couchbase-entity/src/main/java/com/kaufland/generation/model/CommonInterfaceGeneration.kt
@@ -2,6 +2,7 @@ package com.kaufland.generation.model
 
 import com.kaufland.model.entity.BaseEntityHolder
 import com.kaufland.model.entity.WrapperEntityHolder
+import com.kaufland.util.TypeUtil
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.TypeSpec
@@ -11,6 +12,7 @@ class CommonInterfaceGeneration{
     fun generateModel(holder: BaseEntityHolder): FileSpec {
 
         var interfaceSpec = TypeSpec.interfaceBuilder(holder.interfaceSimpleName)
+        interfaceSpec.addSuperinterface(TypeUtil.mapSupport())
 
         var companionSpec = TypeSpec.companionObjectBuilder()
 
