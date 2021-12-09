e1617641eed8e5dd60dd13446e3ccf43f96fef1e
Florian Gubler <florian.gubler@ergon.ch>
2021-12-09 10:04:58 +0100

    Removed unnecessary import

--- couchbase-entity-api/src/main/java/kaufland/com/coachbasebinderapi/PersistenceException.java
+++ couchbase-entity-api/src/main/java/kaufland/com/coachbasebinderapi/PersistenceException.java
@@ -1,7 +1,5 @@
 package kaufland.com.coachbasebinderapi;
 
-import java.lang.Exception;
-
 public class PersistenceException extends Exception {
     public PersistenceException(Throwable var1) {
         super(var1);
