67bfa35d065ed69dc920e29c09207167010cb285
Cyrill Halter <cyrill.halter@ergon.ch>
2024-05-30 07:57:59 +0200

    Add TypeConverter interfaces and annotations

---
+++ crystal-map-api/src/main/java/com/schwarz/crystalapi/TypeConverterImporter.kt
@@ -0,0 +1,7 @@
+package com.schwarz.crystalapi
+
+import kotlin.reflect.KClass
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+annotation class TypeConverterImporter(val typeConverterExporter: KClass<out ITypeConverterExporter>)
